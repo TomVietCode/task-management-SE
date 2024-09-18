9e328053f90c18e8ab10e24b5644b730b96234b3
Nguyen Viet Anh <namchuoi2005@gmail.com>
2024-09-18 07:19:21 +0700

    phan trang

--- server/controllers/task.controller.js
+++ server/controllers/task.controller.js
@@ -14,12 +14,31 @@ module.exports.task = async (req, res) => {
     if(req.query.sortKey && req.query.sortValue){
         sort[req.query.sortKey] = req.query.sortValue
     }
-    
+
     if(req.query.keyword){
         const keyword = new RegExp(req.query.keyword, "i")
         find.title = keyword
     }
 
-    const task = await Task.find(find).sort(sort)
+    const paginationObject = {
+        currentPage: 1, 
+        limitItems: 3
+    }
+
+    if(req.query.page){
+        paginationObject.currentPage = req.query.page
+    }
+
+    paginationObject.skip = (paginationObject.currentPage - 1) * paginationObject.limitItems
+    
+    const count = await Task.countDocuments({
+        delete: false})
+    paginationObject.total = Math.ceil(count / paginationObject.limitItems)
+
+    const task = await Task
+    .find(find)
+    .sort(sort)
+    .skip(paginationObject.skip)
+    .limit(paginationObject.limitItems)
     res.json(task)
 }
